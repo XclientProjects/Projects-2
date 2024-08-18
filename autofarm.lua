4077c7ef824cb7fc66c7790f60e9e07b261d0e81
TheRealAvrwm <161508444+TheRealAvrwm@users.noreply.github.com>
2024-08-18 07:47:46 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,1 +1,20 @@
-print("honestly, yeah")
+while true do
+    local _place = game.PlaceId
+    local _servers = Api .. _place .. "/servers/Public?sortOrder=Asc&limit=100"
+
+    function ListServers(cursor)
+        local Raw = game:HttpGet(_servers .. ((cursor and "&cursor=" .. cursor) or ""))
+        return Http:JSONDecode(Raw)
+    end
+
+    local Server, Next
+    repeat
+        local Servers = ListServers(Next)
+        Server = Servers.data[1]
+        Next = Servers.nextPageCursor
+    until Server
+
+    TPS:TeleportToPlaceInstance(_place, Server.id, game.Players.LocalPlayer)
+
+    wait(10)
+end
